b4c674fbe42231130db7ff468f15d06b1406051a
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-05-23 14:43:38 +0900

    java

---
+++ hy/java/zombe/ColorPoint.java
@@ -0,0 +1,14 @@
+
+public class ColorPoint extends Point{
+	private String color;
+	public void setColor(String color)
+	{
+		this.color = color;
+	}
+	public void showColorPoint()
+	{
+		System.out.println(color);
+		showPoint();
+	}
+
+}
