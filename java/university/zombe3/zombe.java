a5765bdb448616a60eb0f856eaf19a224cb915a5
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-05-31 10:18:18 +0900

    javaZombe3

---
+++ java/university/zombe3/zombe.java
@@ -0,0 +1,21 @@
+package zombe3;
+
+public class zombe extends Unit{
+	
+	public zombe (String name, int pos) 
+	{
+		super(name, pos);
+	}
+	
+	public void move() {
+		int p = r.nextInt(3);
+		if (p == 0) {
+			left();
+		}
+		else if (p==1) {
+			right();
+		}
+		System.out.println(name + "의 현제 위치는"+ pos +"입니다");
+	}
+	
+}
