ed34224e2559d9e3ed20e5c29e9dec272ec6a452
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-06-30 14:24:01 +0900

    python 0630-1

--- Python/home/data crawling/MultipleProduct.py
+++ Python/home/data crawling/MultipleProduct.py
@@ -1,24 +1,27 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import requests
+import csv
 
 browser = webdriver.Chrome()
 URL = "https://search.shopping.naver.com/search/all?query=%EB%B0%80%ED%82%A4%ED%8A%B8%20%EB%96%A1%EB%B3%B6%EC%9D%B4&pagingIndex=1"
 URLTEST = "https://search.shopping.naver.com/search/all?query=%EC%83%9D%EC%88%98&prevQuery=todtn"
 
 response = requests.get(URL)
-html = response.content
+html = browser.page_source
 soup = BeautifulSoup(html, "lxml")
-element = soup.find("a", attrs={"class": "product_link__TrAac linkAnchor"})
+element = soup.find("a", attrs={
+                    "class": "product_link__TrAac linkAnchor", "data-testid": "SEARCH_PRODUCT"})
 price = soup.find("span", attrs={"class": "price_num__S2p_v"})
-elementhref = element.get("href")
+file = open("/users/famelee/desktop/dataset.csv", "w")
+writer = csv.writer(file)
 
-elementList = (soup.find_all(
-    "a", attrs={"class": "product_link__TrAac linkAnchor"}))
+# elementhref = element.get("href")
 
-# print(elementList[0].get_text())
-# print(elementList[1].get_text())
-for elementList in elementList:
-    print(elementList.get_text())
-    print(price.get_text())
-    # print(elementhref)
+# elementList = (soup.find_all(
+#     "a", attrs={"class": "product_link__TrAac linkAnchor"}))
+writer
+
+# for elementList in elementList:
+#     print(elementList.get_text())
+#     print(price.get_text())
--- test/test.py
+++ test/test.py
@@ -3,5 +3,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 browser = webdriver.Chrome()
-start_url = 'https://www.youtube.com/channel/UCtckgmUcpzqGnzcs7xEqMzQ/videos'
-browser.get(start_url)
+
+URL = "https://search.shopping.naver.com/search/all?query=%EB%B0%80%ED%82%A4%ED%8A%B8%20%EB%96%A1%EB%B3%B6%EC%9D%B4&pagingIndex=1"
+
+html = browser.page_source
+print(html)
