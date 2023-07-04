d62cecdc989ee96fe0d677b7d86cb27a28b41af5
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-07-04 18:11:16 +0900

    python 0704-1

--- test/test.py
+++ test/test.py
@@ -1,17 +1,20 @@
+# 모듈 임폴트
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
+# 사용자 입력 받기
 query = input("검색어를 입력하세요: ")
 count = int(input("가져올 상품 수를 입력하세요: "))
 
+# 사이트 주소 생성
 url = "https://search.shopping.naver.com/search/all"
 
 params = {
     "frm": "NVSHATC",
     "origQuery": query,
     "pagingIndex": 1,
-    "pagingSize": count,
+    "pagingSize": 40,
     "productSet": "total",
     "query": query,
     "sort": "rel",
@@ -20,23 +23,30 @@ params = {
 
 res = requests.get(url, params=params)
 
+# 사이트 피싱
 soup = BeautifulSoup(res.text, 'html.parser')
 
 items = soup.select('.basicList_title__3P9Q7 > a')
 prices = soup.select('.price_num__2WUXn')
-links = soup.select('.basicList_title__3P9Q7 > a')
+links = soup.select('.basicList_title_link__1MaTN')
 
+# 결과 처리 및 저장
 results = []
 
-if items:
-    for i in range(count):
-        result = {
-            'name': items[i].text.strip(),
-            'price': prices[i].text.strip().replace(",", ""),
-            'link': links[i].get('href')
-        }
-        results.append(result)
+for i in range(count):
+    if len(items) < i + 1 or len(prices) < i + 1 or len(links) < i + 1:
+        break
 
+    result = {
+        'name': items[i].text.strip(),
+        'price': prices[i].text.strip().replace(",", ""),
+        'link': links[i].get('href')
+    }
+    results.append(result)
+
+if len(results) == 0:
+    print("검색 결과가 없습니다.")
+else:
     wb = Workbook()
     ws = wb.active
     ws.append(['상품명', '가격', '링크'])
@@ -46,5 +56,3 @@ if items:
 
     wb.save(query + ".xlsx")
     print("작업이 완료되었습니다.")
-else:
-    print("검색 결과가 없습니다.")
