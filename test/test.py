7d01fea480a759b143fb59ff64bed29d02d2ab1d
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-09-06 13:31:25 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,58 +1,26 @@
-# 모듈 임폴트
-import requests
-from bs4 import BeautifulSoup
-from openpyxl import Workbook
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
 
-# 사용자 입력 받기
-query = input("검색어를 입력하세요: ")
-count = int(input("가져올 상품 수를 입력하세요: "))
+# 교보문고의 베스트셀러 웹페이지를 가져옵니다.
 
-# 사이트 주소 생성
-url = "https://search.shopping.naver.com/search/all"
+html = urlopen('http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf')
+bsObject = bs(html, "html.parser")
 
-params = {
-    "frm": "NVSHATC",
-    "origQuery": query,
-    "pagingIndex": 1,
-    "pagingSize": 40,
-    "productSet": "total",
-    "query": query,
-    "sort": "rel",
-    "viewType": "list"
-}
+# 책의 상세 웹페이지 주소를 추출하여 리스트에 저장합니다.
+book_page_urls = []
+for cover in bsObject.find_all('div', {'class':'detail'}):
+    link = cover.select('a')[0].get('href')
+    book_page_urls.append(link)
 
-res = requests.get(url, params=params)
+# 메타 정보로부터 필요한 정보를 추출합니다.메타 정보에 없는 저자 정보만 따로 가져왔습니다.   
+for index, book_page_url in enumerate(book_page_urls):
+    html = urlopen(book_page_url)
+    bsObject = bs(html, "html.parser")
+    title = bsObject.find('meta', {'property':'rb:itemName'}).get('content')
+    author = bsObject.select('span.name a')[0].text
+    image = bsObject.find('meta', {'property':'rb:itemImage'}).get('content')
+    url = bsObject.find('meta', {'property':'rb:itemUrl'}).get('content')
+    originalPrice = bsObject.find('meta', {'property': 'rb:originalPrice'}).get('content')
+    salePrice = bsObject.find('meta', {'property':'rb:salePrice'}).get('content')
 
-# 사이트 피싱
-soup = BeautifulSoup(res.text, 'html.parser')
-
-items = soup.select('.basicList_title__3P9Q7 > a')
-prices = soup.select('.price_num__2WUXn')
-links = soup.select('.basicList_title_link__1MaTN')
-
-# 결과 처리 및 저장
-results = []
-
-for i in range(count):
-    if len(items) < i + 1 or len(prices) < i + 1 or len(links) < i + 1:
-        break
-
-    result = {
-        'name': items[i].text.strip(),
-        'price': prices[i].text.strip().replace(",", ""),
-        'link': links[i].get('href')
-    }
-    results.append(result)
-
-if len(results) == 0:
-    print("검색 결과가 없습니다.")
-else:
-    wb = Workbook()
-    ws = wb.active
-    ws.append(['상품명', '가격', '링크'])
-
-    for item in results:
-        ws.append([item['name'], item['price'], item['link']])
-
-    wb.save(query + ".xlsx")
-    print("작업이 완료되었습니다.")
+    print(index+1, title, author, image, url, originalPrice, salePrice)
