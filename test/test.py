e9ae07459f6f0fc3d75c981dd91a861b42f4d63d
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-09-08 08:59:20 +0900

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,18 +1,21 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 
-# 교보문고의 베스트셀러 웹페이지를 가져옵니다.
+
+print("교보문고의 베스트셀러 웹페이지를 가져옵니다.")
 
 html = urlopen('http://www.kyobobook.co.kr/bestSellerNew/bestseller.laf')
 bsObject = bs(html, "html.parser")
 
-# 책의 상세 웹페이지 주소를 추출하여 리스트에 저장합니다.
+print("책의 상세 웹페이지 주소를 추출하여 리스트에 저장합니다.")
+
 book_page_urls = []
 for cover in bsObject.find_all('div', {'class':'detail'}):
     link = cover.select('a')[0].get('href')
     book_page_urls.append(link)
 
-# 메타 정보로부터 필요한 정보를 추출합니다.메타 정보에 없는 저자 정보만 따로 가져왔습니다.   
+print("메타 정보로부터 필요한 정보를 추출합니다.메타 정보에 없는 저자 정보만 따로 가져왔습니다.")
+
 for index, book_page_url in enumerate(book_page_urls):
     html = urlopen(book_page_url)
     bsObject = bs(html, "html.parser")
@@ -23,4 +26,5 @@ for index, book_page_url in enumerate(book_page_urls):
     originalPrice = bsObject.find('meta', {'property': 'rb:originalPrice'}).get('content')
     salePrice = bsObject.find('meta', {'property':'rb:salePrice'}).get('content')
 
+    print("실행완료 결과값을 출력합니다")
     print(index+1, title, author, image, url, originalPrice, salePrice)
