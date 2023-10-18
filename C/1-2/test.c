a244b540fa3c9032e75ff3460d10b674b1b4b9eb
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-10-18 10:42:25 +0900

    js 1018 1

--- C/1-2/test.c
+++ C/1-2/test.c
@@ -3,5 +3,5 @@
 
 void main()
 {
-    printf("fuck")
+    printf("iwantgohome");
 }
---
+++ html/1-2/1-2-5/1.js
@@ -0,0 +1,4 @@
+let strA = '과자 &1500원 & 농심 $2023';
+console.log(strA.split('&'));
+let strb = new String('과자| 1500원');
+console.log('|');
