7334bcf487761273da6f150b71fa6272fc9cbc1a
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-11-20 11:20:33 +0900

    js

--- html/1-2/1-2-6/02.js
+++ html/1-2/1-2-6/02.js
@@ -8,3 +8,25 @@ function age(a) {
 }
 
 age(19)
+
+alba = function (b) {
+    if (a >= 20) {
+        console.log(`당신의 나이가 ${b} 이기 때문에 알바 가능`);
+    }
+    else if (a < 20) {
+        console.log(`당신의 나이가 ${b} 이기 때문에 알바 불가`);
+    }
+}
+
+alba(20)
+
+f = (age1) => 
+{
+    if (a >= 20) {
+        console.log(`당신의 나이가 ${a} 이기 때문에 알바 가능`);
+    }
+    else if (a < 20) {
+        console.log(`당신의 나이가 ${a} 이기 때문에 알바 불가`);
+    }
+}
+
