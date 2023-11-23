b2bd76e36ad46b42d0800c750bcf61d527348fc8
setda1494(최승주) <62476578+setda1494@users.noreply.github.com>
2023-11-23 10:48:31 +0900

    html 1122

--- C/1-2/1-2-10/01.c
+++ C/1-2/1-2-10/01.c
@@ -3,17 +3,6 @@
 #include<stdlib.h>
 typedef struct tnode *tree_pointer;
 
-tree_pointer makeRootNode(int fnum, int score, tree_pointer L, tree_pointer R)
-{
-    tree_pointer ptr = (tree_pointer)malloc(sizeof(struct tnode));
-    ptr->num = fnum;
-    ptr->score = score;
-    ptr->left_child = L;
-    ptr->right_child = R;
-    return ptr; // 수정된 부분: 값을 반환하도록 수정
-}
-
-
 struct tnode
 {
 	tree_pointer left_child;
@@ -24,27 +13,29 @@ struct tnode
 
 // 중위 운행(재귀함수로 작성) 
 // 7 4 10 8 2 1 5 3 9 6
-void inorder(tree_pointer ptr)
+void inorder1(tree_pointer ptr)
 {
-	if (ptr)
-	{
-		inorder1(ptr->left_child);
-		printf("%d", ptr->num);
-		inorder1(ptr->right_child);
-	}
+    if (ptr)
+    {
+        inorder1(ptr->left_child);
+        printf("%d", ptr->num);
+        inorder1(ptr->right_child);
+    }
 }
+
 void inorder2(tree_pointer ptr)
 {
-	if (ptr)
-	{
-		inorder2(ptr->left_child);
-		if (ptr->score >= 800)
-		{
-			printf("%d    %d\n", ptr->num, ptr->score);
-		}
-		inorder2(ptr->right_child);
-	}
+    if (ptr)
+    {
+        inorder2(ptr->left_child);
+        if (ptr->score >= 800)
+        {
+            printf("%d    %d\n", ptr->num, ptr->score);
+        }
+        inorder2(ptr->right_child);
+    }
 }
+
 
 // 전위 운행
 // 1 2 4 7 8 10 3 5  6 9
@@ -84,19 +75,43 @@ tree_pointer makeRootNode(int fnum, int score, tree_pointer L, tree_pointer R)
     ptr->score = score;
     ptr->left_child = L;
     ptr->right_child = R;
+    return ptr; 
 }
+
 
 void main()
 {
-    tree_pointer n10 = makeRootNode(10,700,NULL,NULL);
-    tree_pointer n9 = makeRootNode(9,910,NULL,NULL);
-    tree_pointer n8 = makeRootNode(8,580,n10,NULL);
-    tree_pointer n7 = makeRootNode(7,550,NULL,NULL);
-    tree_pointer n6 = makeRootNode(6,500,n9,NULL);
-    tree_pointer n5 = makeRootNode(5,850,NULL,NULL);
-    tree_pointer n4 = makeRootNode(4,600,n7,n8);
-    tree_pointer n3 = makeRootNode(3,750,n5,n6);
-    tree_pointer n2 = makeRootNode(2,800,n4,NULL);
-    tree_pointer n1 = makeRootNode(1,100,n2,n3);
+    tree_pointer n10 = makeRootNode(10, 700, NULL, NULL);
+    tree_pointer n9 = makeRootNode(9, 900, NULL, NULL);
+    tree_pointer n8 = makeRootNode(8, 910, n10, NULL);
+    tree_pointer n7 = makeRootNode(7, 550, NULL, NULL);
+    tree_pointer n6 = makeRootNode(6, 500, n9, NULL);
+    tree_pointer n5 = makeRootNode(5, 850, NULL, NULL);
+    tree_pointer n4 = makeRootNode(4, 950, n7, n8);
+    tree_pointer n3 = makeRootNode(3, 600, n5, n6);
+    tree_pointer n2 = makeRootNode(2, 750, n4, NULL);
+    tree_pointer n1 = makeRootNode(1, 800, n2, n3);
+
+    printf("Inorder1: ");
+    inorder1(n1);
+    printf("\n");
+
+    printf("Inorder2: ");
+    inorder2(n1);
+    printf("\n");
+
+    printf("Preorder: ");
+    preorder(n1);
+    printf("\n");
+
+    printf("Postorder: ");
+    postorder(n1);
+    printf("\n");
+
 }
 
+
+
+//전위 A B D E F G C H I J K
+//중위 D B F E G A H J I K C
+//후위 D G F E B J J I K H C A
